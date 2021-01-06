d6ebc1ccf5ae7c84eebe6525ee4406b70b7459d4
DitchyTwitchy <piskarev.d.a@gmail.com>
2021-01-06 16:31:59 +0300

    init

---
+++ src/main/java/itsJava/a_Intro/TestClass.java
@@ -0,0 +1,47 @@
+package itsJava.a_Intro;
+
+public class TestClass {
+    private int intParam;
+    private double doubleParam;
+    private String stringParam;
+
+    public TestClass(int intParam, double doubleParam, String stringParam) {
+        this.intParam = intParam;
+        this.doubleParam = doubleParam;
+        this.stringParam = stringParam;
+    }
+
+    public TestClass(int intParam, double doubleParam) {
+        this.intParam = intParam;
+        this.doubleParam = doubleParam;
+    }
+
+    public TestClass(double doubleParam, String stringParam) {
+        this.doubleParam = doubleParam;
+        this.stringParam = stringParam;
+    }
+
+    public int getIntParam() {
+        return intParam;
+    }
+
+    public void setIntParam(int intParam) {
+        this.intParam = intParam;
+    }
+
+    public double getDoubleParam() {
+        return doubleParam;
+    }
+
+    public void setDoubleParam(double doubleParam) {
+        this.doubleParam = doubleParam;
+    }
+
+    public String getStringParam() {
+        return stringParam;
+    }
+
+    public void setStringParam(String stringParam) {
+        this.stringParam = stringParam;
+    }
+}
